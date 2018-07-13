22321dd201b6817769adda0e5a16fe1b454c3872
tz70s <su3g4284zo6y7@gmail.com>
2018-07-13 19:26:44 +0800

    Update build files.

--- ectl/app.js
+++ ectl/app.js
@@ -7,7 +7,7 @@ const deploy = require('./lib/request_handler');
 let path = launchCLI();
 
 source(path).then((sourceObj) => {
-  deploy('http://127.0.0.1:8080/deploy', sourceObj)
+  deploy('http://35.224.21.66/deploy', sourceObj)
     .then((body) => console.log(body))
     .catch((err) => console.error(err));
 });
